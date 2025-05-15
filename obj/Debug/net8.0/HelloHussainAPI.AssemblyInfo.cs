246151ce981ef3aa991ac99262c9ca18a3de6fd2
Hussain Dharwala <hussaindharwala@Hussains-MacBook-Air.local>
2025-05-15 13:00:33 +0530

    newdocker

--- obj/Debug/net8.0/HelloHussainAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloHussainAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloHussainAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90af9b1b7148c4b47926d17d90b5f46a4932d97a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloHussainAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloHussainAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
